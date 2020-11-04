cdd1b12bb02cdd114bc7bfa602a1c6ecc32eee2c
Akhil Mohan <akhil.mohan@mayadata.io>
2020-11-04 11:52:39 +0530

    chore(build): add multiarch support
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

---
+++ Makefile.buildx.mk
@@ -0,0 +1,56 @@
+export DBUILD_ARGS=--build-arg DBUILD_DATE=${DBUILD_DATE} --build-arg DBUILD_REPO_URL=${DBUILD_REPO_URL} --build-arg DBUILD_SITE_URL=${DBUILD_SITE_URL}
+
+ifeq (${TAG}, )
+	export TAG=ci
+endif
+
+# default list of platforms for which multiarch image is built
+ifeq (${PLATFORMS}, )
+	export PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le"
+endif
+
+# if IMG_RESULT is unspecified, by default the image will be pushed to registry
+ifeq (${IMG_RESULT}, load)
+	export PUSH_ARG="--load"
+	# if load is specified, image will be built only for the build machine architecture.
+	export PLATFORMS="local"
+else ifeq (${IMG_RESULT}, cache)
+	# if cache is specified, image will only be available in the build cache, it won't be pushed or loaded
+	# therefore no PUSH_ARG will be specified
+else
+	export PUSH_ARG="--push"
+endif
+
+DOCKERX_IMAGE_PROVISIONER_LOCALPV:=${IMAGE_ORG}/provisioner-localpv:${TAG}
+
+.PHONY: docker.buildx
+docker.buildx:
+	export DOCKER_CLI_EXPERIMENTAL=enabled
+	@if ! docker buildx ls | grep -q container-builder; then\
+		docker buildx create --platform ${PLATFORMS} --name container-builder --use;\
+	fi
+	@docker buildx build --platform ${PLATFORMS} \
+		-t "$(DOCKERX_IMAGE_NAME)" ${DBUILD_ARGS} -f $(PWD)/buildscripts/$(COMPONENT)/$(COMPONENT).Dockerfile \
+		. ${PUSH_ARG}
+	@echo "--> Build docker image: $(DOCKERX_IMAGE_NAME)"
+	@echo
+
+.PHONY: buildx.provisioner-localpv
+buildx.provisioner-localpv: clean
+	@echo "----------------------------"
+	@echo "--> provisioner-localpv    "
+	@echo "----------------------------"
+	@PNAME=${PROVISIONER_LOCALPV} CTLNAME=${PROVISIONER_LOCALPV} BUILDX=true sh -c "'$(PWD)/buildscripts/build.sh'"
+
+.PHONY: docker.buildx.provisioner-localpv
+docker.buildx.provisioner-localpv: DOCKERX_IMAGE_NAME=$(DOCKERX_IMAGE_PROVISIONER_LOCALPV)
+docker.buildx.provisioner-localpv: COMPONENT=$(PROVISIONER_LOCALPV)
+docker.buildx.provisioner-localpv: docker.buildx
+
+.PHONY: buildx.push.provisioner-localpv
+buildx.push.provisioner-localpv:
+	BUILDX=true DIMAGE=${IMAGE_ORG}//provisioner-localpv ./build/push.sh
+
+
+
+
